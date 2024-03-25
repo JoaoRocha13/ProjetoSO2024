ad589f95cd6a6153b35881ee71c4131ec434ae0d
JoaoRocha13 <42808@ufp.edu.pt>
2024-03-25 17:54:30 +0000

    teste

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by so on 25-03-2024.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by so on 25-03-2024.
+//
+
+#ifndef PROJETOSO2024_MAIN_H
+#define PROJETOSO2024_MAIN_H
+
+#endif //PROJETOSO2024_MAIN_H
