6bdb144882c6f6f377778ee43a7c61d9e45e6b72
JoaoRocha13 <42808@ufp.edu.pt>
2024-03-25 21:49:18 +0000

    montecarlo.c

--- main.h
+++ main.h
@@ -5,4 +5,18 @@
 #ifndef PROJETOSO2024_MAIN_H
 #define PROJETOSO2024_MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h> // For POSIX system calls
+#include <fcntl.h>  // For file descriptor based IO
+#include <math.h>
+
+typedef struct {
+    double x;
+    double y;
+} Point;
+#define NUM_POINTS 10000
+
 #endif //PROJETOSO2024_MAIN_H
